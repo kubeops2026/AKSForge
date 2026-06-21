a2e8ac10bebe0d3ef309e47277e9b36f33c0d7f2
kubeops2026 <kubeops2026@gmail.com>
2026-06-21 17:29:26 +0530

    Update var.tf

--- IAC/var.tf
+++ IAC/var.tf
@@ -1,3 +1,81 @@
 variable "subscription_id" {
   type = string
 }
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group where resources will be deployed."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created."
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network."
+  type        = string
+}
+
+variable "address_space" {
+  description = "List of address spaces for the Virtual Network."
+  type        = list(string)
+}
+
+variable "dns_servers" {
+  description = "List of DNS server IP addresses for the Virtual Network."
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Map of tags to assign to resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "subnets" {
+  description = "Map of subnet configurations."
+  type = map(object({
+    address_prefixes = list(string)
+  }))
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "default_node_pool" {
+
+  type = object({
+    name                  = string
+    vm_size               = string
+    node_count            = optional(number)
+    min_count             = optional(number)
+    max_count             = optional(number)
+    enable_auto_scaling   = bool
+    os_disk_size_gb       = optional(number)
+    zones                 = optional(list(string))
+    max_pods              = optional(number)
+    only_critical_addons_enabled = optional(bool)
+  })
+}
+variable "network_profile" {
+
+  type = object({
+    network_plugin      = string
+    network_policy      = optional(string)
+    dns_service_ip      = string
+    service_cidr        = string
+    load_balancer_sku   = string
+    outbound_type       = string
+  })
+}
