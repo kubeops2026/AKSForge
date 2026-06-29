ae5545d663bc48d3df510565b4d203896cd18073
kubeops2026 <kubeops2026@gmail.com>
2026-06-29 21:32:25 +0530

    Update var.tf

--- IAC/var.tf
+++ IAC/var.tf
@@ -1,15 +1,7 @@
 variable "subscription_id" {
   type = string
 }
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group where resources will be deployed."
-  type        = string
-}
 
-variable "location" {
-  description = "Azure region where resources will be created."
-  type        = string
-}
 
 variable "vnet_name" {
   description = "Name of the Virtual Network."
