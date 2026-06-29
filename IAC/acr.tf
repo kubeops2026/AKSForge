4e3e437c2163b234494445335a310e38f7706991
kubeops2026 <kubeops2026@gmail.com>
2026-06-29 21:29:31 +0530

    Update acr.tf

--- IAC/acr.tf
+++ IAC/acr.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "acr" {
-  name                = azurerm_resource_group.rg.name   
+  name                = aks-forge-acr   
   resource_group_name = azurerm_resource_group.rg.location
-  location            = "eastus"
+  location            = azurerm_resource_group.rg.location
 
   sku           = "Standard"           # Basic, Standard, or Premium
   admin_enabled = false
