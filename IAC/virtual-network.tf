232603a904a58f90d739e16a289be42da81d3ad0
kubeops2026 <kubeops2026@gmail.com>
2026-06-29 21:31:29 +0530

    Update virtual-network.tf

--- IAC/virtual-network.tf
+++ IAC/virtual-network.tf
@@ -1,8 +1,8 @@
 module "vnet" {
     source = "../module/child-module/vnet"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
 
-  location = var.location
+  location = azurerm_resource_group.rg.location
 
   vnet_name = var.vnet_name
 
