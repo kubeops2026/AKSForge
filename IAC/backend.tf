e6e444b693dc3c7d056cf92a459970bb40977473
kubeops2026 <kubeops2026@gmail.com>
2026-06-29 21:41:56 +0530

    Create backend.tf

---
+++ IAC/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "aksforgetfss01"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
